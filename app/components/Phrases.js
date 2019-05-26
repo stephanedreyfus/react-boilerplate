aec0a3dfa8a53cf4235e1015a14e36a0e7c1c6b9
Stéphane Dreyfus <floatingrock@gmail.com>
2019-05-25 17:02:02 -0700

    Styling work and add backend server files.

--- app/components/Phrases.js
+++ app/components/Phrases.js
@@ -2,24 +2,27 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.section`
-  background: gray;
-  height: 80%;
+  background: #e0e2e5;
+  min-height: 80%;
   width: 40%;
-  border-radius: 10px;
-  border-width: 3px;
+  border-radius: 6px;
+  border: 0.1em solid mediumvioletred;
+  box-shadow: -2em -2em;
 `;
 
 const DisplayTitle = styled.div`
-  font-family: 'Roboto', sans-serif;
+  font-family: 'Montserrat', sans-serif;
   color: mediumvioletred;
   text-align: center;
-  border-radius: 10px;
-  border-width: 1px;
+  border-bottom: 0.1em solid mediumvioletred;
+  margin: 1em 3em;
 `;
 
 const Display = styled.div`
+  font-family: 'Roboto', sans-serif;
   width: 85%;
-  padding: 1em
+  padding: 1em;
+  margin: 0.5em 1em;
   border-radius: 10px;
   border-width: 1px;
 `;
---
+++ backend/server.js
@@ -0,0 +1,14 @@
+/** A simple back end Express server to connect to a Postrgesql database.
+ * db name = phrases
+ * Test db name = phrases_test */
+
+// const express = require('express');
+
+// //  using process.env to keep private variables, such as secret key, secret
+// require('dotenv').config();
+
+// // Express Middleware
+// const helmet = require('helmet'); // Creates protective headers
+// const bodyParser = require('body-parser'); // For converting response formats
+// const cors = require('cors'); // for controlling SOP
+// const morgan = require('morgan'); // reuest logging
